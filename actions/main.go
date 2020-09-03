6b94b445b01fa4b2d6fab14ce005b79b4ae007dc
soyum2222 <shu123green@qq.com>
2020-09-03 12:48:51 +0800

    commit

--- actions/main.go
+++ actions/main.go
@@ -2,14 +2,52 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/dyweb/gommon/util/httputil"
 	"github.com/google/go-github/v29/github"
 	"golang.org/x/oauth2"
+	"io/ioutil"
 	"os"
 )
 
+func ReadFileToString(filename string) (string, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+type Issue struct {
+	Title    string   `json:"title"`
+	Body     string   `json:"body"`
+	Labels   []string `json:"labels"`
+	Assignee []string `json:"assignee"`
+}
+
 func main() {
+
+
+	var cfg string
+
+	flag.StringVar(&cfg,"cfg","","")
+
+
+	data ,err:=ioutil.ReadFile(cfg)
+	if err!=nil{
+		panic(err)
+	}
+
+
+	issueInfo := Issue{}
+	err = json.Unmarshal(data,&issueInfo)
+	if err!=nil {
+		panic(err)
+	}
+
 
 	var token = os.Getenv("GITHUB_TOKEN")
 
@@ -23,17 +61,11 @@ func main() {
 
 	client := github.NewClient(hc)
 
-	title := "测试自动开issues"
-	assignee := []string{"soyum2222"}
-
-	body := "测试自动开issues:"
 	req := &github.IssueRequest{
-		Title: &title,
-		Labels: &[]string{
-			"bug",
-		},
-		Assignees: &assignee,
-		Body:      &body,
+		Title:     &issueInfo.Title,
+		Labels:    &issueInfo.Labels,
+		Assignees: &issueInfo.Assignee,
+		Body:      &issueInfo.Body,
 	}
 
 	issue, _, err := client.Issues.Create(context.Background(), "soyum2222", "actions", req)
